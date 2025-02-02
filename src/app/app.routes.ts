b2771d524e894c1af1a282d035f9644664f989d5
diegoarnanz-maker <diegoarnanzlozano96@gmail.com>
2025-02-02 20:11:00 +0100

    Npm de bootstrap y fontawesome, layout creado y componentes para estructura del proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { UsuarioFormComponent } from './pages/usuario-form/usuario-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'home'
+    },
+    {
+        path: 'home',
+        component: HomeComponent
+    },
+    {
+        path: 'newuser',
+        component: UsuarioFormComponent
+    }
+];
